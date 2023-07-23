b7840e96afe032deb2fe7d0cb6b23d7518eb4751
NewGithub20223 <121712055+NewGithub20223@users.noreply.github.com>
2023-07-23 13:12:57 +0800

    Add files via upload

---
+++ easyload.py
@@ -0,0 +1,34 @@
+# load 1: ...
+# load 2: ⟹⟹⟹
+# load 3: >>>
+
+from sys import stdout
+import time
+import os
+
+class loadbar:
+    def load1(self):
+        for i in range(5):
+            print("please wait", end="")
+            time.sleep(0)
+            self.write("...")
+            os.system("cls")
+    def load2(self):
+        for i in range(5):
+            print("loading: ", end="")
+            time.sleep(0)
+            self.write("⟹⟹⟹")
+            os.system("cls")
+    def load3(self):
+        for i in range(5):
+            print("starting ", end="")
+            time.sleep(0)
+            self.write(">>>")
+            os.system("cls")
+    def write(self, a):
+        for i in a:
+            stdout.write(i)
+            stdout.flush()
+            time.sleep(0.5)
+class 
+load = loadbar()
